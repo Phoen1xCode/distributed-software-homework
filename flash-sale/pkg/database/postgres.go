3b3af76fca96a8d86a330e512eb56c1d3c6712e2
Phoen1xCode <cnphoen1xcode@gmail.com>
2026-03-11 14:58:48 +0800

    feat: add GORM PostgreSQL database connection

---
+++ flash-sale/pkg/database/postgres.go
@@ -0,0 +1,30 @@
+package database
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+func NewPostgresDB(dsn string) (*gorm.DB, error) {
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info), // Enable detailed logging
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to PostgreSQL: %w", err)
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get sql.DB from gorm.DB: %w", err)
+	}
+	sqlDB.SetMaxOpenConns(100)
+	sqlDB.SetMaxIdleConns(10)
+
+	log.Println("Connected to PostgreSQL")
+	return db, nil
+}
